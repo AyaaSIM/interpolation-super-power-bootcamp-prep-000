0b05922dacb2b9d1ec515a58e205d3b2838c1d5b
Ayaa <ayaa1siddig5@gmail.com>
2018-08-07 02:18:07 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,13 +1,11 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
-  if colors=['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
+ colors=['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
   print "#{colors[0]}, "
   print "#{colors[1]}, " 
   print "#{colors[2]}, "
   print "#{colors[3]}, "
   print "#{colors[4]}, "
   print "#{colors[5]}, "
-  print "#{colors[6]}"
-else " "
-end
-end
+  print "#{colors[6]}"]
+  end
